1e82227904c2b8a66c78afe70798cdf2e1794f75
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2019-07-19 17:01:45 +0200

    Change list to table

--- Daftcode-zadanie3/tablica.js
+++ Daftcode-zadanie3/tablica.js
@@ -1,18 +1,18 @@
 function filterFunction() {
-  let input, filter, ul, li, a, i, txtValue;
+  let input, filter, table, td, a, i, txtValue;
   input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+  table = document.getElementById("myTable");
+  td = table.getElementsByTagName('td');
 
   
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
+  for (let i = 0, row; row = table.rows[i]; i++) {
+    a = table.rows[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      table.rows[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      table.rows[i].style.display = "none";
     }
   }
 }
